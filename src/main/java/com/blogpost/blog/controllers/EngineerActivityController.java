aa8262a893564fd1a0e3186b32cf84f830293707
unknown <VELOX@Affan-Malik.velox.co.in>
2024-10-19 12:45:43 +0530

    engineeractivityadded

--- src/main/java/com/blogpost/blog/controllers/EngineerActivityController.java
+++ src/main/java/com/blogpost/blog/controllers/EngineerActivityController.java
@@ -3,9 +3,13 @@ package com.blogpost.blog.controllers;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogpost.blog.entities.Ticket;
@@ -15,10 +19,13 @@ import com.blogpost.blog.entities.Ticket;
 @RequestMapping("/api/engineer")
 public class EngineerActivityController {
 	
-	
 	@GetMapping("/getAllEngineerTicket")
-	public List<Ticket> getAllEngineerTicket()
+	public List<Ticket> getAllEngineerTicket( HttpServletRequest httpServletRequest,HttpSession httpSession)
 	{
+		 String username = httpSession.getAttribute("username").toString();
+		 String Company = 	httpSession.getAttribute("Company").toString();
+		 String GroupName = httpSession.getAttribute("GroupName").toString();
+		 
 		return null;
 	}
 }
